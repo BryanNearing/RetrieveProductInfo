88fa5ca98d1a16f85a905d3e9c115edd7ab0ffaf
BryanNearing <bryannearing@gmail.com>
2020-12-18 16:00:43 -0500

    testing

--- src/com/Servlets/RetrieveProductId.java
+++ src/com/Servlets/RetrieveProductId.java
@@ -31,6 +31,7 @@ public class RetrieveProductId extends HttpServlet {
 		// TODO Auto-generated method stub
 		int id = Integer.parseInt(request.getParameter("ID"));
 		
+		
 		Product p = ProductIdDao.getRecordById(id);
 		
 		if(p != null)
